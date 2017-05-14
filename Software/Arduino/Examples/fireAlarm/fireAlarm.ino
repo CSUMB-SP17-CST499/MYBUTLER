255ad942098ea01933223764dd423a8dc5e8e932
Andrew Porter <APorter@CSUMB.edu>
2017-05-14 11:50:29 -0700

    Major scripts update

--- Software/Arduino/Examples/fireAlarm/fireAlarm.ino
+++ Software/Arduino/Examples/fireAlarm/fireAlarm.ino
@@ -17,12 +17,12 @@
 #include <Wire.h>
 
 // Define GPIO Ports
-#define BUTTON_PIN 0
-#define LED_PIN 2
+#define BUTTON_PIN 4
+#define LED_PIN 16
 
 // Set Wi-Fi Paramaters
-const char* WIFI_SSID = "REPLACE ME WITH YOUR SSID";
-const char* WIFI_PASS = "REPLACE ME WITH YOUR PASS";
+const char* WIFI_SSID = "YOUR SSID HERE";
+const char* WIFI_PASS = "YOUR PASS HERE";
 
 // Set MQTT Parameters
 const char* MQTT_BROKER = "192.168.1.200";
@@ -35,48 +35,52 @@ const char* CONTROL_TOPIC = "/firealarm/control";
 // Heartbeat and State Tracking Variables
 unsigned long PREV_HB_TIME = 0;
 int HB_INTERVAL = 1000;
-bool ALARM_STATE = false;
+int debounceCounter = 0;
 bool BUTTON_STATE = false;
-char REPORT_VALUE = '0';
+bool ALARM_STATE = false;
+char* REPORT_VALUE = "0";
 
 void handleStateChar(char _input) {
   
-    // Get a boolean input type and apply to ALARM_STATE
-    ALARM_STATE = false;
     // Convert the incoming char into a bool
+	ALARM_STATE = false;
     if (_input == '1') {
         ALARM_STATE = true;
     }
-  
-}
-
-// State Machine updating based on physical system
-void updateLED() {
-
-	bool state = false;
-	if(ALARM_STATE){
-		state = (millis() % 200) > 100;
-	}
-	
-	digitalWrite(LED_PIN, state);
 	
 }
 
 // State Machine updating based on physical system
-void updateButton() {
-  
-	int counter = 0;
-	for( int i = 0; i <= 5; i++){
-		if(digitalRead(BUTTON_PIN)){
-			counter++;
+void setState() {
+
+	// Simple debounce to translate BUTTON_PIN into BUTTON_STATE
+    if (digitalRead(BUTTON_PIN)) {
+        if(debounceCounter == 10){
+			BUTTON_STATE = true;
 		}
-		delay(5);
-	}
-	BUTTON_STATE = false;
-	if(counter>2){
-		BUTTON_STATE = true;
-	}
+		else{
+			debounceCounter++;
+		}
+    } else {
+        if(debounceCounter == 0){
+			BUTTON_STATE = false;
+		}
+		else{
+			debounceCounter--;
+		}
+    }
   
+
+  
+}
+
+void updateLED(){
+	if( ((millis()%100)>50) && ALARM_STATE ){
+		digitalWrite(LED_PIN, LOW); // ON
+	}
+	else{
+		digitalWrite(LED_PIN, HIGH); // OFF
+	}
 }
 
 // Call this when a message is received - MQTT unpacking and message handling
@@ -91,6 +95,10 @@ void messageHandler(char * _topic, byte * _payload, unsigned int _length) {
         for (int i = 0; i < _length; i++) {
             // Dispatch incoming char to handler
             handleStateChar(_payload[i]);
+            // Extend next heartbeat and update CURRENT_STATE
+            PREV_HB_TIME = millis() + 1000;
+            delay(50);
+            setState();
         }
     }
   
@@ -103,14 +111,14 @@ PubSubClient MQTT_CLIENT(MQTT_BROKER, MQTT_PORT, messageHandler, WIFI_CLIENT);
 // Heartbeat function
 int heartbeat() {
 	
-	// Convert ALARM_STATE bool into char for outgoing message
-    REPORT_VALUE = '0';
+	// Convert CURRENT_STATE bool into char for outgoing message
+    REPORT_VALUE[0] = '0';
     if (BUTTON_STATE) {
-        REPORT_VALUE = '1';
+        REPORT_VALUE[0] = '1';
     }
 	
 	// Attempt to publish the REPORT_VALUE on the STATUS_TOPIC and return success
-    if (MQTT_CLIENT.publish(STATUS_TOPIC, &REPORT_VALUE)) {
+    if (MQTT_CLIENT.publish(STATUS_TOPIC, REPORT_VALUE)) {
         Serial.print("HB, State: ");
         Serial.println(REPORT_VALUE);
         return 1;
@@ -127,11 +135,9 @@ void reconnect() {
   
     // Loop until we're reconnected
     while (!MQTT_CLIENT.connected()) {
-	
         Serial.print("Connecting to MQTT Broker...");
-		
         // Attempt to connect
-        if ( MQTT_CLIENT.connect("mqttClientID") ) {
+        if ( MQTT_CLIENT.connect("fire") ) {
             Serial.println("connected!");
             // When connected, subscribe to CONTROL_TOPIC
             MQTT_CLIENT.subscribe(CONTROL_TOPIC);
@@ -155,10 +161,9 @@ void setup() {
     Serial.println();
 
     // Initialize Pin Directions/Levels
-    pinMode(RELAY_PIN, OUTPUT);
-    digitalWrite(RELAY_PIN, HIGH);
-    pinMode(STATE_PIN, OUTPUT); // PULLUP/PULLDOWN FUNCTION - use as input when not programming ESP-01 breakout
-    digitalWrite(STATE_PIN, LOW); // PULLUP/PULLDOWN FUNCTION - delete if using STATE_PIN as input
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, HIGH); // OFF
+    pinMode(BUTTON_PIN, INPUT);
 
     // Setup Wi-Fi
     WiFi.mode(WIFI_STA);
@@ -187,15 +192,14 @@ void loop() {
     }
     // Handle MQTT Pub/Sub back-end functions
     MQTT_CLIENT.loop();
-	
-	// Handle high-frequency state machine updates
-	updateButton();
-	updateLED();
 
     // If hearbeat timer is exceeded, perform needed actions and update timer
     if ( millis() > (PREV_HB_TIME + HB_INTERVAL)) {
         heartbeat();
         PREV_HB_TIME = millis();
     }
+	
+    setState();
+	updateLED();
 
 }
---
+++ Software/Arduino/Examples/motionSensor/motionSensor.ino
@@ -0,0 +1,141 @@
+/*
+*
+*	MY-BUTLER Motion Sensor Program for ESP8266
+*	
+*	This program uses MQTT to interface with a motion sensor
+*	and broadcasts the result to the broker.
+*
+*	The program is meant to be used with an ESP-01 module, but if
+*	using another format, simply replace pin numbers as needed and
+*	remove sections that perform pullup/pulldown functions.
+*
+*/
+
+#include <EEPROM.h> 
+#include <ESP8266WiFi.h> 
+#include <PubSubClient.h> 
+#include <Wire.h>
+
+// Define GPIO Ports
+#define BUTTON_PIN 0
+#define LED_RED_PIN 1 // Serial pin - cannot use Serial in sketch!
+#define LED_GRN_PIN 3 // Serial pin - cannot use Serial in sketch!
+
+// Set Wi-Fi Paramaters
+const char* WIFI_SSID = "MY-BUTLER";
+const char* WIFI_PASS = "ABCDE12345";
+
+// Set MQTT Parameters
+const char* MQTT_BROKER = "192.168.1.200";
+const int MQTT_PORT = 1883;
+const char* MQTT_USER = "";
+const char* MQTT_PASS = "";
+const char* STATUS_TOPIC = "/motion/status";
+
+// Heartbeat and State Tracking Variables
+unsigned long PREV_HB_TIME = 0;
+int HB_INTERVAL = 1000;
+bool CURRENT_STATE = false;
+
+// State Machine updating based on physical system
+void setState() {
+  
+	// Set CURRENT_STATE according to actual state of BUTTON_PIN
+    if (digitalRead(BUTTON_PIN)) {
+        CURRENT_STATE = true;
+    } else {
+        CURRENT_STATE = false;
+    }
+	digitalWrite(LED_RED_PIN, CURRENT_STATE);
+	digitalWrite(LED_GRN_PIN, !CURRENT_STATE);
+  
+}
+
+// Call this when a message is received - MQTT unpacking and message handling
+void messageHandler(char * _topic, byte * _payload, unsigned int _length) {
+
+    // This should never happen!
+
+  
+}
+
+// Create Wi-Fi and MQTT clients
+WiFiClient WIFI_CLIENT;
+PubSubClient MQTT_CLIENT(MQTT_BROKER, MQTT_PORT, messageHandler, WIFI_CLIENT);
+
+// Heartbeat function
+int heartbeat() {
+	
+	// Convert CURRENT_STATE bool into char for outgoing message
+    char* REPORT_VALUE = "0";
+    if (CURRENT_STATE) {
+        REPORT_VALUE[0] = '1';
+    }
+	
+	// Attempt to publish the REPORT_VALUE on the STATUS_TOPIC and return success
+    if (MQTT_CLIENT.publish(STATUS_TOPIC, REPORT_VALUE)) {
+        return 1;
+    } else {
+        return 0;
+    }
+  
+}
+
+// Reconnect to the MQTT Broker
+void reconnect() {
+  
+    // Loop until we're reconnected
+    while (!MQTT_CLIENT.connected()) {
+        // Attempt to connect
+        if ( MQTT_CLIENT.connect("motion") ) {
+            // Good to go!
+        } 
+		// If connection failed, alert and retry after cooldown
+		else {
+            delay(2000);
+        }
+    }
+  
+}
+
+void setup() {
+
+    // Set LED pins
+    pinMode(LED_GRN_PIN, OUTPUT);
+    digitalWrite(LED_GRN_PIN, LOW);
+    pinMode(LED_RED_PIN, OUTPUT);
+    digitalWrite(LED_RED_PIN, LOW);
+	
+	// Set button pin
+    pinMode(BUTTON_PIN, OUTPUT); // PULLUP/PULLDOWN FUNCTION - use as input when not programming ESP-01 breakout
+    digitalWrite(BUTTON_PIN, LOW); // PULLUP/PULLDOWN FUNCTION - delete if using STATE_PIN as input
+
+    // Setup Wi-Fi
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+    // Try to connect to Wi-Fi until successful
+    while ( WiFi.status() != WL_CONNECTED ) {
+        delay(500);
+    }
+
+  
+}
+
+void loop() {
+  
+    // Connect to the MQTT Broker if needed
+    if ( !MQTT_CLIENT.connected() ) {
+        reconnect();
+    }
+    // Handle MQTT Pub/Sub back-end functions
+    MQTT_CLIENT.loop();
+
+    // If hearbeat timer is exceeded, perform needed actions and update timer
+    if ( millis() > (PREV_HB_TIME + HB_INTERVAL)) {
+        setState();
+        heartbeat();
+        PREV_HB_TIME = millis();
+    }
+
+}
